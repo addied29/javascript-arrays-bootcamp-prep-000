a66366e79393a9682f1b0c38293e981cbf3e180a
Addie Dean <addiedean@gmail.com>
2019-04-16 15:53:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function accessElementInArray(array,index){
     return array;
   }
   function removeElementFromBeginningOfArray(array){
-    array.slice(0)
+    array.slice(1)
     return array;
   }
   function destructivelyRemoveElementFromEndOfArray(array){
